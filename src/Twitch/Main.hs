1e7967575946696ce16ee5db16d81c1df26feb8e
Jonathan Fischoff <jonathangfischoff@gmail.com>
2014-11-14 21:24:29 -0800

    doc

--- src/Twitch/Main.hs
+++ src/Twitch/Main.hs
@@ -182,6 +182,17 @@ optionsToConfig Options {..} = do
         }
   return (currentDir', config, mhandle)
 
+-- | Simplest way to create a file watcher app. Set your main equal to defaultMain 
+--   and you are good to go. In this example
+--   
+--   > {-# LANGUAGE QuasiQuotes #-}
+--   > import Twitch 
+--   > import FileCommand
+--   >
+--   > main = defaultMain $ do
+--   >   "blog.md"    |> [s|pandoc -o$directory$basename.html $path|]
+--   >   "blog.html"  |> [s|osascript refreshSafari.AppleScript|]
+-- 
 defaultMain :: Dep -> IO ()
 defaultMain dep = do
   let opts = info (helper <*> pOptions)
