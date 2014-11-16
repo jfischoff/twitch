219bc8f588fca199a74f8207103a7c2c9fe8a70b
Jonathan Fischoff <jonathangfischoff@gmail.com>
2014-11-15 21:36:36 -0800

    Update Twitch.hs

--- src/Twitch.hs
+++ src/Twitch.hs
@@ -1,11 +1,11 @@
--- | Twitch is monadic DSL and library for file watching. 
+-- | Twitch is a monadic DSL and library for file watching. 
 --   It conveniently utilizes 'do' notation in the style of 
 --   <https://hackage.haskell.org/package/shake Shake> and 
 --   <https://hackage.haskell.org/package/clay clay> to expose the functionality of the
 --   <http://hackage.haskell.org/package/fsnotify fsnotify> cross-platform file system 
 --   watcher.
 --   
---   Here is an example that converts Markdown files to Html and reloads Safari
+--   Here is an example that converts Markdown files to HTML and reloads Safari
 --   whenever the input files change.
 --   
 --   > {-# LANGUAGE OverloadedStrings #-}
@@ -17,23 +17,23 @@
 --   >   "*.html" |> \_ -> system $ "osascript refreshSafari.AppleScript"
 --   
 --   Rules are specified in the 'Dep' (for Dependency) monad. The library takes advantage 
---   of the OverloadedStrings extension to create a Dep value from a glob pattern.
+--   of the OverloadedStrings extension to create a 'Dep' value from a glob pattern.
 --   
 --   After creating a 'Dep' value using a glob, event callbacks are added using prefix
 --   or infix API.
 --   
---   There are three types of events, \'add\', \'modify\' and \'delete\'. In many cases, 
---   the add and modify responses are the same, so an \'add and modify\' API 
+--   There are three types of events: \'add\', \'modify\' and \'delete\'. In many cases, 
+--   the \'add\' and \'modify\' responses are the same, so an \'add and modify\' API 
 --   is provided
 --   
---   In the example above an \'add and modify\' callback was added to both the \"*.md\" 
+--   In the example above, an \'add and modify\' callback was added to both the \"*.md\" 
 --   and \"*.html\" globs using the '|>' operator. 
 --   
---   All this is the common case, differing callbacks can be added with '|+' (or 'add')
+--   Although this is the common case, differing callbacks can be added with '|+' (or 'add')
 --   and '|%' (or 'modify') functions. Finally, delete callbacks are added with 
 --   '|-' (of 'delete').
 --   
---   Here is a more complex usage example, handling all three events seperately.
+--   Here is a more complex usage example, handling all three events separately.
 --   
 --   > handleHaskellFiles :: Dep 
 --   > handleHaskellFiles = "src/**/*.hs" |+ addToCabalFile |% reloadFile |- removeFromCabalFile
@@ -44,8 +44,7 @@
 --   library's documentation.
 --   
 --   Since a command pattern is calling system commands with a file path, a useful addition
---   to twitch is the <https://hackage.haskell.org/package/file-command-qq-0.1.0.4 file-command-qq> quasiquoter, 
---   which is the package of the same name. 
+--   to twitch is the <https://hackage.haskell.org/package/file-command-qq-0.1.0.4 file-command-qq> quasiquoter. 
 --
 --   Here is a slightly more complicated version the example from earlier, using the 
 --   FileCommand quasiquoter.
