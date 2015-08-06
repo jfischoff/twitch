f9c9532539a41d7ce0c24bdc7761cef8c7f4c050
Andreas Schacker <menelaos@users.noreply.github.com>
2015-08-06 21:00:38 +0200

    Add test for `modHeadRule`

--- tests/Tests/Twitch/Internal.hs
+++ tests/Tests/Twitch/Internal.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Tests.Twitch.Internal where
 import Test.Hspec
+import Twitch.Internal ( modHeadRule, runDep )
 
 tests :: Spec
-tests = return ()
+tests = do
+  describe "modHeadRule" $ it "should thread state correctly" $ do
+    let patterns  = ["*.md", "*.html", "*.hs", "*.cpp", "*.js"]
+        deps      = fmap (flip modHeadRule id) patterns
+    (length . runDep $ sequence_ deps) `shouldBe` length patterns
