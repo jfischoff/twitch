e7e2a9f0e970db6a4618e1f59d2237ef1719f412
Andreas Schacker <menelaos@users.noreply.github.com>
2015-08-10 11:08:58 +0200

    More exact assertion

--- tests/Tests/Twitch/Internal.hs
+++ tests/Tests/Twitch/Internal.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module Tests.Twitch.Internal where
+import Control.Monad ( forM_ )
 import Test.Hspec
 import Twitch.Internal ( modHeadRule, runDep )
+import Twitch.Rule ( Rule (pattern) )
 
 tests :: Spec
 tests = do
   describe "modHeadRule" $ it "should thread state correctly" $ do
-    let patterns  = ["*.md", "*.html", "*.hs", "*.cpp", "*.js"]
-        deps      = fmap (flip modHeadRule id) patterns
-    (length . runDep $ sequence_ deps) `shouldBe` length patterns
+    -- `patterns` is used as `[Rule]` and `[Dep]` here
+    let patterns = ["*.md", "*.html", "*.hs", "*.cpp", "*.js"]
+        dep      = forM_ patterns (flip modHeadRule id)
+    fmap pattern (runDep dep) `shouldBe` fmap pattern (reverse patterns)
